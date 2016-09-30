0a71464ac9bb22c480f1569a75392189141e01b7
AnaMaria <aungureanu614@gmail.com>
2016-09-30 17:20:51 +0000

    networking pictionary

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var express = require('express');
+var socket_io = require('socket.io');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+
+io.on('connection', function(socket){
+    socket.on('draw', function(position){
+        socket.broadcast.emit('draw', position);
+    });
+});
+
+server.listen(process.env.PORT || 8080);
